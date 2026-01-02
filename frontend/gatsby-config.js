2e14dc1c491535f1b5cbd8846acc509114dc2354
Lutfiandraa <lutfiandra30042004@gmail.com>
2026-01-02 15:38:41 +0700

    vercel

--- frontend/gatsby-config.js
+++ frontend/gatsby-config.js
@@ -8,8 +8,20 @@
  * @type {import('gatsby').GatsbyConfig}
  */
 module.exports = {
+  siteMetadata: {
+    title: `Karta Jati Integrasi`,
+    description: `Solusi Integrasi Teknologi dan Digitalisasi Kebutuhan anda`,
+    siteUrl: `https://kartajati.co.id`,
+  },
   plugins: [
     `gatsby-plugin-postcss`,
     `gatsby-plugin-typescript`,
   ],
+  flags: {
+    FAST_DEV: true,
+    PRESERVE_WEBPACK_CACHE: false,
+    PRESERVE_FILE_DOWNLOAD_CACHE: false,
+    PARALLEL_SOURCING: false,
+    DETECT_NODE_MUTATIONS: false,
+  },
 }
